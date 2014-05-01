08e9c61a30f4b55ad6bd4d8d75d494d867a03d05
BDNelson123 <ben.david.nelson@gmail.com>
2014-05-01 15:55:08 +0000

    most of the front-pages working

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var house = angular.module('house', ['ngRoute', 'ngAnimate', 'ngResource']);
 
 house.config(function ($routeProvider) {
   $routeProvider
-    .when ('/login', {templateUrl: "templates/login.html"})
-    .when ('/home', {templateUrl: "templates/home.html", controller: 'homeController'})
-    .when ('/about', {templateUrl: "templates/about.html"})
-    .when ('/', {templateUrl: "templates/home.html", controller: 'homeController'})
+    .when ('/public/home', {templateUrl: "templates/public/home.html", controller: 'homeController'})
+    .when ('/public/how_it_works', {templateUrl: "templates/public/how_it_works.html"})
+    .when ('/public/pricing', {templateUrl: "templates/public/pricing.html"})
+    .when ('/public/contact', {templateUrl: "templates/public/contact.html"})
+    .when ('/public/career', {templateUrl: "templates/public/career.html"})
+    .when ('/users/sign_in', {templateUrl: "templates/users/sign_in.html"})
+    .when ('/users/new', {templateUrl: "templates/users/new.html"})
+    .when ('/', {templateUrl: "templates/public/home.html", controller: 'homeController'})
     .otherwise({ redirectTo: '/' });
 });
