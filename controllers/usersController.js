77ce6718202c1dc4ef37186651338dec9e6658a7
BDNelson123 <ben.david.nelson@gmail.com>
2014-05-29 19:23:48 +0000

    user authentication working

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,7 @@
 house.controller('usersController', function($scope, newUser) {
   $scope.submitUser = function() {
     return newUser.save(
-      { post: $scope.post }
+      { user: $scope.user }
     );
   };
 });
