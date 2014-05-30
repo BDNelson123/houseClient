714390506cece0b4a5a4419d792832fa8b880802
BDNelson123 <ben.david.nelson@gmail.com>
2014-05-30 18:21:28 +0000

    now logging responses and requestes with interceptor

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-var house = angular.module('house', ['ngRoute', 'ngAnimate', 'ngResource']);
+var house = angular.module('house', ['ngRoute', 'ngAnimate', 'ngResource'])
+.config(function ($provide, $httpProvider) {
+  $provide.factory('MyHttpInterceptor', function ($q) {
+    return {
+      request: function (config) {
+        console.log(config); 
+        return config || $q.when(config);
+      },
+
+      requestError: function (rejection) {
+        console.log(rejection);
+        return $q.reject(rejection);
+      },
+ 
+      response: function (response) {
+        console.log(response);
+        return response || $q.when(response);
+      },
+
+      responseError: function (rejection) {
+        console.log(rejection);
+        return $q.reject(rejection);
+      }
+    };
+  });
+ 
+  $httpProvider.interceptors.push('MyHttpInterceptor');
+});
+
 var server = "http://198.61.208.215:3000";
 
 house.config(function ($routeProvider) {
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,25 @@
-house.controller('usersController', function($scope, newUser) {
+house.controller('usersController', function($scope, newUser, $location) {
   $scope.submitUser = function() {
-    return newUser.save(
+    newUser.save(
       { user: $scope.user }
     );
+
+    function success(response) {
+      console.log("success", response);
+      $location.path("/contacts");
+    }
+
+    function failure(response) {
+      console.log("failure", response)
+
+      _.each(response.data, function(errors, key) {
+        _.each(errors, function(e) {
+          $scope.form[key].$dirty = true;
+          $scope.form[key].$setValidity(e, false);
+        });
+      });
+
+      $location.path('/public/contact');
+    }
   };
 });
