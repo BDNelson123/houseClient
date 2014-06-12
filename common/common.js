391ca8bcfe9c28c39bcab4d6bb179232480b17e3
BDNelson123 <ben.david.nelson@gmail.com>
2014-06-12 15:03:59 +0000

    users now redirected to sign in if not logged in

--- common/common.js
+++ common/common.js
@@ -4,7 +4,7 @@ var server = "http://198.61.208.215:4000";
 // restrict access to logged in users
 function restrict_access(location,token){
   if(!token) {
-    location.path('/');
+    location.path('/users/sign_in');
   }
 }
 
