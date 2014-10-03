17d75894e82cd312ae0edd0d91f586a9525ddc01
BDNelson123 <ben.david.nelson@gmail.com>
2014-10-03 21:09:31 +0000

    continued overhaul of UI

--- directives/user.js
+++ directives/user.js
@@ -15,26 +15,26 @@ house.directive('ngUseroptions', function() {
           <div class="btn-group btn-group-justified">\
             <div class="btn-group">\
               <a href="/#/users/show/{{ngUser}}?type=activity">\
-                <button type="button" class="btn btn-default active" ng-show="ngRoute == \'activity\'">Activity</button>\
-                <button type="button" class="btn btn-default active" ng-if="!ngRoute">Activity</button>\
+                <button type="button" class="btn btn-primary active" ng-show="ngRoute == \'activity\'">Activity</button>\
+                <button type="button" class="btn btn-primary active" ng-if="!ngRoute">Activity</button>\
                 <button type="button" class="btn btn-default" ng-if="ngRoute" ng-show="ngRoute != \'activity\'">Activity</button>\
               </a>\
             </div>\
             <div class="btn-group">\
               <a href="/#/users/show/{{ngUser}}?type=listings">\
-                <button type="button" class="btn btn-default active" ng-show="ngRoute == \'listings\'">Listings</button>\
+                <button type="button" class="btn btn-primary active" ng-show="ngRoute == \'listings\'">Listings</button>\
                 <button type="button" class="btn btn-default" ng-hide="ngRoute == \'listings\'">Listings</button>\
               </a>\
             </div>\
             <div class="btn-group">\
               <a href="/#/users/show/{{ngUser}}?type=bids">\
-                <button type="button" class="btn btn-default active" ng-show="ngRoute == \'bids\'">Bids</button>\
+                <button type="button" class="btn btn-primary active" ng-show="ngRoute == \'bids\'">Bids</button>\
                 <button type="button" class="btn btn-default" ng-hide="ngRoute == \'bids\'">Bids</button>\
               </a>\
             </div>\
             <div class="btn-group">\
               <a href="/#/users/show/{{ngUser}}?type=messages">\
-                <button type="button" class="btn btn-default active" ng-show="ngRoute == \'messages\'">Messages</button>\
+                <button type="button" class="btn btn-primary active" ng-show="ngRoute == \'messages\'">Messages</button>\
                 <button type="button" class="btn btn-default" ng-hide="ngRoute == \'messages\'">Messages</button>\
               </a>\
             </div>\
