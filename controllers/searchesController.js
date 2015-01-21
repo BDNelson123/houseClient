cefedb76b327955d0c558ddde354048217c16d28
BDNelson123 <ben.david.nelson@gmail.com>
2015-01-21 23:03:06 +0000

    pagination done - needs refactor

--- controllers/searchesController.js
+++ controllers/searchesController.js
@@ -6,7 +6,32 @@ house.controller('searchesController', function($scope, $location, $routeParams,
   };
 
   $scope.searchHome = function() {
-    $scope.searches = indexSearch.read({query: $routeParams.query});
+    // total number of search results (not paginated)
     $scope.count = indexSearch.read({query: $routeParams.query, count: "true"});
+    $scope.query = $routeParams.query;
+
+    // get total number of search results
+    $scope.count.$promise.then(function(data) {
+      var pages = Math.ceil($scope.count[0][0] / 10);
+
+      // actual search query && pagination
+      if($routeParams.page != null && $routeParams.page <= pages){
+        $scope.currentPage = parseInt($routeParams.page); // get the current page user is on
+        $scope.searches = indexSearch.read({query: $routeParams.query, page: $routeParams.page});
+      } else {
+        $scope.currentPage = 1; // get the current page user is on
+        $scope.searches = indexSearch.read({query: $routeParams.query, page: 1});
+      }
+
+      // this loop builds the pages array we loop over in the view to see how many pages we have
+      var pages_array = [];
+      for (i = 0; i < pages; i++) {
+        pages_array.push(i + 1);
+      }
+      $scope.pages = pages_array;
+      $scope.lastPage = Math.max.apply(Math, pages_array);
+      $scope.prevPage = $scope.currentPage - 1;
+      $scope.nextPage = $scope.currentPage + 1;
+    });
   };
 });
